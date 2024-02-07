018e3bc80bcd186044f2e2a86eb54ca7063626b4
ABHAYB642 <abhay.biramane@mmit.edu.in>
2024-02-07 20:48:59 +0530

    Web Services for our Project

---
+++ Spring_Boot_Web_Service/src/main/java/com/demo/controller/CommentController.java
@@ -0,0 +1,61 @@
+package com.demo.controller;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import com.demo.models.Comment;
+import com.demo.service.CommentService;
+
+
+@RestController
+@CrossOrigin("*")
+public class CommentController {
+	@Autowired
+	CommentService cservice;
+	
+	@GetMapping("/comments")
+	public ResponseEntity<List<Comment>> getallComments(){
+		List<Comment> clist=cservice.getAllComments();
+		return ResponseEntity.ok(clist);	
+	}
+	
+	@GetMapping("/comment/{cid}")
+	public ResponseEntity<Comment> getById(@PathVariable String cid){
+		Comment c=cservice.getCommentById(cid);
+		if (c!=null)
+			return ResponseEntity.ok(c);
+		else
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+	}
+	
+	@PostMapping("comment/{cid}")
+	public ResponseEntity<String> addNewComment(@RequestBody Comment c){
+		cservice.addNewComment(c);
+		return ResponseEntity.ok("Comment added successfully");
+	}
+	
+	@PutMapping("comment/{cid}")
+	public ResponseEntity<String> updateComment(@RequestBody Comment c){
+		cservice.updateCommentById(c);
+		return ResponseEntity.ok("Comment updated successfully");
+	}
+	
+	@DeleteMapping("/comment/{cid}")
+	public ResponseEntity<String> DeleteComment(@PathVariable String cid){
+		cservice.deleteCommentById(cid);
+		return ResponseEntity.ok("Comment deleted successfully");	
+	}
+}
+
+
+
+
+
---
+++ Spring_Boot_Web_Service/src/main/java/com/demo/service/IngredientServiceImpl.java
@@ -0,0 +1,74 @@
+package com.demo.service;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.demo.dao.IngredientDao;
+import com.demo.models.Ingredient;
+import com.demo.models.Recipe;
+
+
+@Service
+public class IngredientServiceImpl implements IngredientService {
+	
+	@Autowired
+	private IngredientDao idao;
+
+	@Override
+	public List<Ingredient> getAllIngredients() {
+		return idao.findAll();
+	}
+
+	@Override
+	public Boolean addNewIngredient(Ingredient i) {
+		if(idao.save(i) != null) {
+			 return true;
+		   }
+			return false;
+		}
+
+	@Override
+	public Ingredient getIngredientById(int ingid) {
+		Optional<Ingredient> op=idao.findById(ingid);
+		 if(op.isPresent()) {
+			 return op.get();
+		 }
+		 return null;
+	}
+
+	@Override
+	public Boolean updateIngredientById(Ingredient i) {
+		Optional<Ingredient> op=idao.findById(i.getIngredientId());
+		if(op.isPresent()) {
+			Ingredient i1=op.get();
+			i1.setIngredientName(i.getIngredientName());
+			i1.setIngredientType(i.getIngredientType());
+			i1.setCalorieCount(i.getCalorieCount());
+			i1.setRecipeIngredients(i.getRecipeIngredients());
+			i1.setRecipeIngredients(i.getRecipeIngredients());
+			if(idao.save(i1)!=null) {
+				return true;
+			};
+		}
+		return false;
+	}
+
+	@Override
+	public Boolean deleteIngredientById(int ingid) {
+		if (idao.existsById(ingid)) {
+			idao.deleteById(ingid);
+			return true;
+		}
+		return false;
+	}
+	
+	public Set<Recipe> getRecipesByIngredient(int ingredientId) {
+	    Ingredient ingredient = idao.findById(ingredientId).orElse(null);
+	    if (ingredient != null) {
+	        return ingredient.getRecipeIngredients();
+	    } else {
+	        return null;
+	    }
+	}
+}
