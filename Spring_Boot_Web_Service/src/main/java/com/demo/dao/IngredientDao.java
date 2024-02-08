a470c0955922e4ba96f3bc38ec1d2a36132194cb
ABHAYB642 <abhay.biramane@mmit.edu.in>
2024-02-08 21:18:38 +0530

    Web Services Update

--- Spring_Boot_Web_Service/src/main/java/com/demo/dao/IngredientDao.java
+++
@@ -1,7 +0,0 @@
-package com.demo.dao;
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.demo.models.Ingredient;
-
-public interface IngredientDao extends JpaRepository<Ingredient,Integer> {
-
-}
