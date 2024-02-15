defeea88a586d62a39261652540b04f3f891cf2d
SAK-11 <sakshipawar1114@gmail.com>
2024-02-15 13:10:00 +0530

    newfiles

--- FF_Frontend/srcmealplan/App.js
+++ FF_Frontend/srcmealplan/App.js
@@ -1,36 +1,43 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import "./App.css";
-import Footer from "./components/footer";
-
-import MealPlanning from "./pages/MealPlanning";
+import React, { useState } from "react";
+import MealList from "./MealList";
 
 function App() {
+  const [mealData, setMealData] = useState(null);
+  const [calories, setCalories] = useState(2000);
+
+  function getMealData() {
+    fetch(
+      `https://api.spoonacular.com/mealplanner/generate?apiKey=cb1c464d94f142c08b156c5beddade8b&timeFrame=day&targetCalories=${calories}`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setMealData(data);
+      })
+      .catch(() => {
+        console.log("error");
+      });
+  }
+
+  function handleChange(e) {
+    setCalories(e.target.value);
+  }
+
   return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <div>
-               <MealPlanning/>
-              </div>
-            }
-          />
-          <Route
-            path="/registration.js"
-            element={
-              <>
-               
-              <MealPlanning/>
-                <Footer />
-              </>
-            }
-          />
-        </Routes>
-      </BrowserRouter>
-    </>
+    <div className="App">
+      <section className="controls">
+        <input
+          type="number"
+          placeholder="Calories (e.g. 2000)"
+          onChange={handleChange}
+        />
+        <button onClick={getMealData}>Get Daily Meal Plan</button>
+      </section>
+      {mealData && <MealList mealData={mealData} />}
+    </div>
   );
 }
 
 export default App;
+
+
+ //`https://api.spoonacular.com/mealplanner/generate?apiKey= bc658907e5bf40f59036876b8bc0a54d&timeFrame=day&targetCalories=${calories}`
